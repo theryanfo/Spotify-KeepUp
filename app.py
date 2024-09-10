02df1e544eab8c217e878160f221f1db6477ac08
theryanfo <ryan.s.fordham@gmail.com>
2024-09-09 20:40:53 -0700

    successfully retrieved first 50 songs data

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv, dotenv_values
 import os
+import time
 
 load_dotenv()
 
@@ -12,11 +13,13 @@ app.secret_key = os.getenv("SECRET_KEY")
 app.config['SESSION_COOKIE_NAME'] = 'RyansCookie'
 TOKEN_INFO = "token_info"
 
+
 @app.route('/')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
+
 
 @app.route('/redirect')
 def redirectPage():
@@ -25,11 +28,33 @@ def redirectPage():
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session[TOKEN_INFO] = token_info
-    return 'Redirect'
+    return redirect(url_for('getTracks', _external=True))
+
 
 @app.route('/getTracks')
 def getTracks():
-    return 'Tracks Page'
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect("/")
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    data = sp.current_user_saved_tracks(limit=50, offset=0)
+    return data
+
+
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
+
 
 def create_spotify_oauth():
     return SpotifyOAuth(
